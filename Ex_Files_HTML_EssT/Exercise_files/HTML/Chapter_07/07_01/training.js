6c5dcdc317c53256071109e89200ea5452b3a9e2
scottroylance <docroylance@hotmail.com>
2015-02-06 16:22:51 -0800

    JavaScript

---
+++ Ex_Files_HTML_EssT/Exercise_files/HTML/Chapter_07/07_01/training.js
@@ -0,0 +1,3 @@
+    if (style.linked) {
+        h2 {<a href="https://www.lds.org/?lang=eng" title="surprise link" target="_blank">};
+    }
--- Ex_Files_HTML_EssT/Exercise_files/JavaScript/Ex_Files_JS_EssT_2011/Exercise Files/Chap05/AccessingDOM/script.js
+++ Ex_Files_HTML_EssT/Exercise_files/JavaScript/Ex_Files_JS_EssT_2011/Exercise Files/Chap05/AccessingDOM/script.js
@@ -1,2 +1,10 @@
+var maintitle = document.getElementById("mainTitle");
 
+console.log("This is an element of type: ", mainTitle.nodeType);
+console.log("the Inner HTML is " ,mainTitle.innerHTML);
+console.log("Child nodes: ", mainTitle.childNodes.length);
 
+var myLinks = document.getElementsByTagName("a");
+console.log("Links: ", myLinks.length);
+
+console.log("Apparently, these logs are a quick way to survey the website to learn more about it. Yay...")
